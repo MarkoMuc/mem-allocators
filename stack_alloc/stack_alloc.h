f6958923b1cd1363704511f6241eaeb7b0242d92
Marko Z. Muc <marko.muc12@gmail.com>
2025-03-09 20:13:13 +0100

    seperated stack alloc

--- stack_alloc/stack_alloc.h
+++ stack_alloc/stack_alloc.h
@@ -33,21 +33,23 @@ typedef struct Stack_Allocation_Header Stack_Allocation_Header;
 typedef struct Strict_Stack Strict_Stack;
 typedef struct Strict_Stack_Alloc_Header Strict_Stack_Alloc_Header;
 
+struct Stack {
+    unsigned char *buf;
+    size_t buf_len;
+    size_t offset;
+};
+
+struct Stack_Allocation_Header {
+    uint8_t padding;
+};
+
 bool is_power_of_two(uintptr_t x);
 size_t calc_padding_with_header(uintptr_t ptr, uintptr_t alignment, size_t header_size);
 void *stack_alloc_align(Stack *s, size_t size, size_t alignment);
 void *stack_resize_align(Stack *s, void *ptr, size_t old_size, size_t new_size, size_t alignment);
-void *strict_stack_alloc_align(Strict_Stack *s, size_t size, size_t alignment);
-void *strict_stack_resize_align(Strict_Stack *s, void *ptr, size_t old_size, size_t new_size, size_t alignment);
 
 void *stack_alloc(Stack *s, size_t size);
 void *stack_resize(Stack *s, void *ptr, size_t old_size, size_t new_size);
 void stack_free(Stack *s, void *ptr);
 void stack_free_all(Stack *s);
 void stack_init(Stack *s, void *backing_buffer, size_t backing_buffer_length);
-
-void *strict_stack_alloc(Strict_Stack *s, size_t size);
-void *strict_stack_resize(Strict_Stack *s, void *ptr, size_t old_size, size_t new_size);
-void strict_stack_free(Strict_Stack *s, void *ptr);
-void strict_stack_free_all(Strict_Stack *s);
-void strict_stack_init(Strict_Stack *s, void *backing_buffer, size_t backing_buffer_length);
