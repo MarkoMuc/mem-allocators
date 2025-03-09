8b74f1cf3056cfa80c8f73b2b8427991af6cbb88
Marko Z. Muc <marko.muc12@gmail.com>
2025-03-09 16:39:57 +0100

    strict stack

--- stack_alloc/stack_alloc.c
+++ stack_alloc/stack_alloc.c
@@ -10,6 +10,18 @@ struct Stack_Allocation_Header {
     uint8_t padding;
 };
 
+struct Strict_Stack {
+    unsigned char *buf;
+    size_t buf_len;
+    size_t prev_offset;
+    size_t curr_offset;
+};
+
+struct Strict_Stack_Alloc_Header {
+    size_t prev_offset;
+    size_t padding;
+};
+
 inline bool is_power_of_two(uintptr_t x) {
     return (x & (x-1)) == 0;
 }
@@ -150,4 +162,96 @@ void stack_init(Stack *s, void *backing_buffer, size_t backing_buffer_length) {
     s->buf = (unsigned char*)backing_buffer;
     s->buf_len = backing_buffer_length;
     s->offset = 0;
-}:w
+}
+
+void *strict_stack_alloc_align(Strict_Stack *s, size_t size, size_t alignment) {
+    uintptr_t curr_addr, next_addr;
+    size_t padding;
+    Strict_Stack_Alloc_Header *header;
+
+    if(alignment > 128) {
+        // The padding is 8 bits, the largest alignment can be used is 128 bytes.
+        alignment = 128;
+    }
+
+    curr_addr = (uintptr_t)s->buf + (uintptr_t)s->curr_offset;
+    padding = calc_padding_with_header(curr_addr, (uintptr_t)alignment, sizeof(Strict_Stack_Alloc_Header));
+    if(s->curr_offset + padding + size > s->buf_len) {
+        return NULL;
+    }
+
+    s->prev_offset = s->curr_offset;
+    s->curr_offset += padding;
+
+    next_addr = curr_addr + (uintptr_t)padding;
+    header = (Strict_Stack_Alloc_Header *)(next_addr - sizeof(Strict_Stack_Alloc_Header));
+    header->padding = (uint8_t)padding;
+    header->prev_offset = s->prev_offset;
+
+    s->curr_offset += size;
+
+    return memset((void *)next_addr, 0, size);
+}
+
+void *strict_stack_resize_align(Strict_Stack *s, void *ptr, size_t old_size, size_t new_size, size_t alignment) {
+    if(ptr == NULL) {
+        return strict_stack_alloc_align(s, new_size, alignment);
+    } else if (new_size == 0) {
+        strict_stack_free(s, ptr);
+        return NULL;
+    } else {
+        uintptr_t start, end, curr_addr, prev_offset;
+        size_t min_size = old_size < new_size? old_size : new_size;
+        Strict_Stack_Alloc_Header *header;
+        void *new_ptr;
+
+        start = (uintptr_t)s->buf;
+        end = start + (uintptr_t)s->buf_len;
+        curr_addr = (uintptr_t)ptr;
+
+        if(!(start <= curr_addr && curr_addr < end)) {
+            assert(0 && "Out of bounds memory address passed to stack allocator (resize)");
+            return NULL;
+        }
+
+        header = (Strict_Stack_Alloc_Header *)(curr_addr - sizeof(Strict_Stack_Alloc_Header));
+        prev_offset = (size_t)(curr_addr - (uintptr_t)header->padding - start);
+
+        if(prev_offset != header->prev_offset) {
+            assert(0 && "Out of order stack allocator free");
+            return NULL;
+        }
+
+        if (old_size == min_size) {
+            return ptr;
+        }
+
+        new_ptr = strict_stack_alloc_align(s, new_size, alignment);
+        memmove(new_ptr, ptr, min_size);
+
+        s->curr_offset = s->prev_offset;
+        s->prev_offset = header->prev_offset;
+
+        return new_ptr;
+    }
+}
+
+void *strict_stack_resize(Strict_Stack *s, void *ptr, size_t old_size, size_t new_size) {
+    return strict_stack_resize_align(s, ptr, old_size, new_size, DEFAULT_ALIGNMENT);
+}
+
+void *strict_stack_alloc(Strict_Stack *s, size_t size) {
+    return strict_stack_alloc_align(s, size, DEFAULT_ALIGNMENT);
+}
+
+void strict_stack_free_all(Strict_Stack *s) {
+    s->curr_offset = 0;
+    s->prev_offset = 0;
+}
+
+void strict_stack_init(Strict_Stack *s, void *backing_buffer, size_t backing_buffer_length) {
+    s->buf = (unsigned char *) backing_buffer;
+    s->buf_len = backing_buffer_length;
+    s->curr_offset = 0;
+    s->prev_offset = 0;
+}
